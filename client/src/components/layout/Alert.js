20e1f221f7121489786f420e36c083ba8cece6e0
aaronweggemans <aaronweggemans@hotmail.nl>
2020-05-17 13:52:37 +0200

    Section 8

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -5,16 +5,11 @@ import { connect } from 'react-redux';
 const Alert = ({ alerts }) =>
   alerts !== null &&
   alerts.length > 0 &&
-  alerts.map(
-    (alert) => (
-      console.log(alert),
-      (
-        <div key={alert.id} className={`alert alert-${alert.alertType}`}>
-          {alert.msg}
-        </div>
-      )
-    )
-  );
+  alerts.map((alert) => (
+    <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+      {alert.msg}
+    </div>
+  ));
 
 Alert.propTypes = {
   alerts: PropTypes.array.isRequired,
